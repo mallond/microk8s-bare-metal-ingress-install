35b8698cf2e04ad5a9402cbe3e616d05be44228d
David Mallon ⚙ <mallond@gmail.com>
2021-03-18 09:21:50 -0700

    Update microk8s_create_services.sh

--- microk8s_create_services.sh
+++ microk8s_create_services.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 sudo kubectl apply -f https://raw.githubusercontent.com/mallond/microk8s/main/service_apple.yaml
 sudo kubectl apply -f https://raw.githubusercontent.com/mallond/microk8s/main/service_banana.yaml
