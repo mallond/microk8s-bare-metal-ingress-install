de438f86d77f81849a050256a7885086b16f3545
David Mallon ⚙ <mallond@gmail.com>
2021-03-18 09:13:53 -0700

    Update microk8sbuild.sh

--- microk8sbuild.sh
+++ microk8sbuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# sudo snap install microk8s --classic
+sudo snap install microk8s --classic
 sudo microk8s status --wait-ready
 sudo microk8s enable dashboard dns registry ingress
 sudo snap alias microk8s.kubectl kubectl
