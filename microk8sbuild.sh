a47dac9dbdf1340a8d7e969ea7754b6bb735851e
David Mallon ⚙ <mallond@gmail.com>
2021-03-18 10:32:50 -0700

    Update microk8sbuild.sh

--- microk8sbuild.sh
+++ microk8sbuild.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo 'Installing microk8s... make take a few minutes'
 sudo snap install microk8s --classic
 sudo microk8s status --wait-ready
 sudo microk8s enable dashboard dns registry ingress
