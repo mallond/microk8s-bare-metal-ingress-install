c7dec0f14b4a806a2db4559376333b494f841321
David Mallon ⚙ <mallond@gmail.com>
2021-05-18 14:45:09 -0700

    Update and rename build_services.sh to build_step2_services.sh

--- build_step2_services.sh
+++ build_step2_services.sh
@@ -2,4 +2,5 @@
 sudo kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.44.0/deploy/static/provider/baremetal/deploy.yaml
 sudo kubectl apply -f https://raw.githubusercontent.com/mallond/microk8s/main/service_apple.yaml
 sudo kubectl apply -f https://raw.githubusercontent.com/mallond/microk8s/main/service_banana.yaml
+sudo kubectl apply -f https://raw.githubusercontent.com/mallond/microk8s/main/influxdb.yaml
 sudo kubectl apply -f https://raw.githubusercontent.com/mallond/microk8s/main/service_ingress.yaml
